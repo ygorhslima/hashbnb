cbd3a71ccd83c70f62e121b6c13b028e8eb5112c
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-12-03 09:13:48 -0300

    .

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header=()=>{
+    return(
+        <div>
+            <div className="flex items-center">
+                <img src="" alt="" />
+                <a href="">ashbnb</a>
+            </div>
+        </div>
+    )
+}
+export default Header
