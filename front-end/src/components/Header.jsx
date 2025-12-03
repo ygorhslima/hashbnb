5466681d3c75c4facc135bf549c6472a6868e3df
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-12-03 09:48:24 -0300

    implementando o cabeçalho do site

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,11 +1,70 @@
 const Header=()=>{
     return(
-        <div>
-            <div className="flex items-center">
-                <img src="" alt="" />
-                <a href="">ashbnb</a>
+        <header className="shadow-md">
+            <div className="flex items-center justify-between px-8 py-2 max-w-7xl mx-auto">
+                <div className="flex items-center">
+                    <img
+                        src="https://cdn.prod.website-files.com/61b9e0dd381626819c8d4f83/65e2198d48039ba6444f602b_logo%20hashtag%20-%20h.webp"
+                        alt="Logo da Hashtag"
+                        className="max-h-10"
+                    />
+                    <a href="" className="text-3xl font-bold text-red-400">ashbnb</a>
+                </div>
+                <div className="flex items-center border border-gray-300 px-4 py-2 rounded-full shadow-md">
+                    <p className="border-r border-r-gray-300 pr-4">Qualquer lugar</p>
+                    <p className="border-r border-r-gray-300 px-4">Qualquer Semana</p>
+                    <p className="px-4">Qualquer Hóspedes</p>
+                
+                    <div className="bg-red-400 rounded-full p-2 text-white">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-4"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                            />
+                        </svg>
+                    </div>
+                </div>
+
+                <div className="flex items-center border border-gray-300 px-4 py-2 rounded-full shadow-md gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-5 text-gray-600"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                        />
+                    </svg>
+
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="size-5 text-gray-600"
+                    >
+                        <path
+                        fillRule="evenodd"
+                        d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                        clipRule="evenodd"
+                        />
+                    </svg>
+
+                    <p className="px-4">Diego XYZ</p>
+                </div>
             </div>
-        </div>
+        </header>
     )
 }
 export default Header
